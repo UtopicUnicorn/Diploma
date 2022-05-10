19eb34130b323b65f98b12053b946032e6b55151
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-05-10 16:20:51 +0700

    data picker

---
+++ components/CustomPicker.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {Alert, Modal, TouchableOpacity, View, Text} from "react-native";
+import {Picker} from "@react-native-picker/picker";
+
+export const CustomPicker = ({
+    modalOpen,
+    setModalOpen,
+    value,
+    setValue,
+    items,
+}: any) =>{
+    const pickerData = (data: any) => {
+        return (data?.length > 0)&&(
+            data.map((val:any, index:any) => <Picker.Item label={val} value={val} key={index}/>)
+        );
+    };
+    return(
+        <Modal
+            animationType={"slide"}
+            transparent={true}
+            visible={modalOpen}
+            onRequestClose={()=>{
+                Alert.alert('Modal has been closed!');
+            }}
+        >
+            <View style={{flex:1}}>
+                <View style={{
+                    backgroundColor:'#fff',
+                    width:'100%',
+                    height:'40%',
+                    position:'absolute',
+                    bottom:0,
+                }}>
+                    <TouchableOpacity
+                        style={{
+                            justifyContent: 'center',
+                            alignItems:'center'
+                        }}
+                        onPress={()=>setModalOpen(!modalOpen)}
+                    >
+                        <Text>Закрыть</Text>
+                    </TouchableOpacity>
+                    <Picker
+                        selectedValue={value}
+                        style={{height: 50, width:'100%'}}
+                        onValueChange={(itemValue, itemIndex) =>
+                            setValue(itemValue)
+                        }>
+                        {pickerData(items)}
+                    </Picker>
+                </View>
+            </View>
+        </Modal>
+    )
+}
